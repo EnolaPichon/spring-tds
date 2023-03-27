52ba3be0ea96ff70c6045890d28f0df24bc15d90
Enola <enola.pichon@etu.unicaen.fr>
2023-03-27 15:44:32 +0200

    Teste

--- src/main/kotlin/edu/spring/td1/ServletInitializer.kt
+++ src/main/kotlin/edu/spring/td1/ServletInitializer.kt
@@ -6,7 +6,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-		return application.sources(Td1Application::class.java)
+		return application.sources(Td1Application::class.java).
+
 	}
 
 }
